4fbf4c1d56b6cbebff7abddaf9471e4eed52009e
Niged01 <nigedoberer@gmail.com>
2022-08-02 10:05:12 +0000

    Refactored Email form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-            emailjs.send("service_jso9n11", "3Z5vcxPriZOgq75ki", "template_lclscqr", {
+            emailjs.send("service_jso9n11", "template_lclscqr", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
